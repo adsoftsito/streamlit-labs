1300decd714a2be03899fe7af29f2a2fabb1d2eb
adsoftsito <adsoft@live.com.mx>
2022-11-10 12:54:37 -0600

    agregue programas de button, list, text

--- cache.py
+++ cache.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 st.title("Streamlit con cache")
 
-DATA_URL = 'dataset.csv'
+#DATA_URL = 'https://raw.githubusercontent.com/adsoftsito/streamlit-labs/master/dataset.csv?token=GHSAT0AAAAAAB2NR7KYLAP4VXUGLOVWKVL6Y3NHLEQ'
+DATA_URL =  'https://raw.githubusercontent.com/AngelCavazos/streamlit-labs/master/dataset.csv?token=GHSAT0AAAAAAB2XYZDNMM6IXFT5C2TPCA7UY3NFUYQ'
 
 @st.cache
 def load_data(nrows):
@@ -11,7 +12,7 @@ def load_data(nrows):
     return data
 
 data_load_state = st.text('Loading data ...')
-data = load_data(500)
+data = load_data(90000)
 data_load_state.text("Done ...")
 
 st.dataframe(data)
