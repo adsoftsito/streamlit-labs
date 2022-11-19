bc239d1f856f126b8e638bb5a643cc2940d979fd
adsoftsito <adsoft@live.com.mx>
2022-11-18 23:03:35 -0600

    Update m5.py

--- m5.py
+++ m5.py
@@ -1,111 +1,166 @@
 # manuel.py
-import pandas as pd
+# Jorge
+#Importando librerias necesarias
 import streamlit as st
-import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import codecs
 
-names_link = 'Employees.csv'
-names_data = pd.read_csv(names_link,nrows=500)
+st.title('Análisis de datos de deserción')
+st.text('Aplicación para el análisis de la deserción en empresas de las ciudades del país.')
 
 sidebar = st.sidebar
-
-st.title("Modulo 5 Streamlit")
-st.header("Employees")
-st.text("Analisando datos")
-
-sidebar.title("Sidebar")
-showHide = sidebar.checkbox("Mostrar DataFrame")
-
-if showHide:
-  st.dataframe(names_data)
+sidebar.title("Jorge - Filtros de búsqueda")
 
 
-def load_data_bytitle(title):
-    print(title)
-    print(names_data['Hometown'].head(1).str.upper())
-    filtered_data_bytit = names_data[names_data['Hometown'].str.upper().str.contains(title) | names_data['Unit'].str.upper().str.contains(title) | names_data['Employee_ID'].str.upper().str.contains(title)]
-    print(filtered_data_bytit)
-    return filtered_data_bytit	
+# DATE_COLUMN = 'started_at'
+DATA_URL = ('Employees.csv')
 
-
-tit = sidebar.text_input("Buscar")
-
-if (tit):
-  print(tit)
-  filterbytitle = load_data_bytitle(tit.upper())
-  count_row = filterbytitle.shape[0]
-  st.title('Coincidencias')
-  st.write(f"Total Titles: {count_row}")
-  st.dataframe(filterbytitle)
-
-selected_sex = sidebar.selectbox("Nivel educativo", names_data['Education_Level'].unique())
-selected_unit = sidebar.selectbox("Unit", names_data['Unit'].unique())
-selected_city = sidebar.selectbox("Ciudad", names_data['Hometown'].unique())
-
+#Funcion para obtener todos los datos
 @st.cache
-def load_data_edl(dat):
-  filtered_data_byDir = names_data[names_data["Education_Level"] == dat]
-  return filtered_data_byDir
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    # data.rename({'start_lat': 'lat', 'start_lng': 'lon'}, axis=1, inplace=True)
+    # data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 
+#Funcion para obtener datos filtrados
 @st.cache
-def load_data_unit(dat):
-  filtered_data_byDir = names_data[names_data["Unit"] == dat]
-  return filtered_data_byDir
+def load_data_byVar(name,valor):
+    doc = codecs.open(DATA_URL,'rU','latin1')
+    data = pd.read_csv(doc)
+    lowercase = lambda x: str(x).lower()
+    if valor == 1:
+        filtered_data_byVar = data[data['Employee_ID'].str.contains(name)]
+    elif valor == 2:
+        filtered_data_byVar = data[data['Hometown'].str.contains(name)]
+    elif valor == 3:
+        filtered_data_byVar = data[data['Unit'].str.contains(name)]
+    else: 
+        filtered_data_byVar = data[data['Education_Level'] == int(name)]
+    return filtered_data_byVar
 
-@st.cache
-def load_data_city(dat):
-  filtered_data_byDir = names_data[names_data["Hometown"] == dat]
-  return filtered_data_byDir
 
-if selected_sex:
-	filterbydir = load_data_edl(selected_sex)
-	count_row = filterbydir.shape[0]
-	st.write(f"Total Items: {count_row}")
-	st.dataframe(filterbydir)
-	st.markdown("___")
 
-if selected_unit:
-	filterbydir = load_data_unit(selected_unit)
-	count_row = filterbydir.shape[0]
-	st.write(f"Total Items: {count_row}")
-	st.dataframe(filterbydir)
-	st.markdown("___")
+#Muestra todos los datos
+data = load_data(500)
+mcheckbox = False
+mcheckbox = st.sidebar.checkbox('Mostrar dataframe completo')
+if mcheckbox:
+    st.header('Conjuntos de datos')
+    st.subheader('Todos los datos')
+    st.write(data)
 
-if selected_city:
-	filterbydir = load_data_city(selected_city)
-	count_row = filterbydir.shape[0]
-	st.write(f"Total Items: {count_row}")
-	st.dataframe(filterbydir)
-	st.markdown("___")
 
-#Edad
-fig, ax = plt.subplots()
-plt.title("Edad Histograma")
-ax.hist(names_data["Age"], bins=20)
-st.pyplot(fig)
+### ---------Text inputs y Botones
+id_empleado = sidebar.text_input('Id de Empleado:')
+btnIdEmpleado = sidebar.button('Buscar')
+if(btnIdEmpleado):
+    st.header('Conjuntos de datos')
+    mcheckbox = False
+    st.subheader('Filtro por Empleado')
+    filterbyId = load_data_byVar(id_empleado,1)
+    st.dataframe(filterbyId)
+    count_row = filterbyId.shape[0]
+    st.write(f"Total Empleados : {count_row}")
 
-#Unit
-fig2, ax2 = plt.subplots()
-plt.title("Unidad Histograma")
-ax2.hist(names_data.groupby('Unit').size(), bins=20)
-st.pyplot(fig2)
+hometown = sidebar.text_input('Ciudad de Origen:')
+btnhometown = sidebar.button('Buscar por Ciudad')
+if(btnhometown):
+    st.header('Conjuntos de datos')
+    mcheckbox = False
+    st.subheader('Filtro por Ciudad')
+    filterbyhometown = load_data_byVar(hometown,2)
+    st.dataframe(filterbyhometown)
+    count_row = filterbyhometown.shape[0]
+    st.write(f"Total Empleados : {count_row}")
 
-#Ciudad
-ht = names_data[['Hometown','Attrition_rate']].groupby('Hometown').max()
-fig3, ax3 = plt.subplots()
-plt.title("Ciudad con tasa de desercion Histograma")
-ax3.hist(ht['Attrition_rate'], bins=20)
-st.pyplot(fig3)
+unidad = sidebar.text_input('Unidad :')
+btnunidad = sidebar.button('Buscar por Unidad')
+if(btnunidad):
+    st.header('Conjuntos de datos')
+    mcheckbox = False
+    st.subheader('Filtro por Unidad')
+    filterbyunidad = load_data_byVar(unidad,3)
+    st.dataframe(filterbyunidad)
+    count_row = filterbyunidad.shape[0]
+    st.write(f"Total Empleados : {count_row}")
 
-#Edad
-edad = names_data[['Age','Attrition_rate']].groupby('Age').max()
-fig4, ax4 = plt.subplots()
-plt.title("Edad con tasa de desercion Histograma")
-ax4.hist(edad['Attrition_rate'], bins=20)
-st.pyplot(fig4)
 
-#Tiempo de servicio
-tiempo = names_data[['Time_of_service','Attrition_rate']].groupby('Time_of_service').max()
-fig5, ax5 = plt.subplots()
-plt.title("Tiempo de servicio con tasa de desercion Histograma")
-ax5.hist(tiempo['Attrition_rate'], bins=20)
-st.pyplot(fig5)
+###---------Selectboxs y botones
+doc = codecs.open(DATA_URL,'rU','latin1')
+data = pd.read_csv(doc)
+education_level = sidebar.selectbox("Selecciona el Nivel Educativo", data['Education_Level'].sort_values().unique())
+btnEducation = sidebar.button('Busca por nivel educativo: ')
+if(btnEducation):
+    st.header('Conjuntos de datos')
+    mcheckbox = False
+    st.subheader('Filtro por Nivel Educativo')
+    filterbyEdu = load_data_byVar(education_level,4)
+    st.dataframe(filterbyEdu)
+    count_row = filterbyEdu.shape[0]
+    st.write(f"Total Empleados : {count_row}")
+
+
+
+#doc = codecs.open(DATA_URL,'rU','latin1')
+#data = pd.read_csv(doc)
+ciudad = sidebar.selectbox("Selecciona la ciudad", data['Hometown'].unique())
+btnciudad = sidebar.button('Busca por ciudad: ')
+if(btnciudad):
+    st.header('Conjuntos de datos')
+    mcheckbox = False
+    st.subheader('Filtro por Ciudad')
+    filterbyTown = load_data_byVar(ciudad,2)
+    st.dataframe(filterbyTown)
+    count_row = filterbyTown.shape[0]
+    st.write(f"Total Empleados : {count_row}")
+
+
+# doc = codecs.open(DATA_URL,'rU','latin1')
+# data = pd.read_csv(doc)
+unidadS = sidebar.selectbox("Selecciona la Unidad", data['Unit'].sort_values().unique())
+btnUnidadS = sidebar.button('Busca Unidad: ')
+if(btnUnidadS):
+    st.header('Conjuntos de datos')
+    mcheckbox = False
+    st.subheader('Filtro por Unidad')
+    filterbyUnit = load_data_byVar(unidadS,3)
+    st.dataframe(filterbyUnit)
+    count_row = filterbyUnit.shape[0]
+    st.write(f"Total Empleados : {count_row}")
+
+
+
+###---------Gráficos
+st.header('Gráficos')
+# Histograma
+st.subheader('Histograma por edades')
+hist_values = np.histogram(data['Age'],bins=20, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# Empleados por Unidad
+st.subheader('Empleados por Unidad')
+bar_values = data.loc[:,['Unit','Employee_ID']].groupby('Unit').count()
+bar_values.rename(columns= {'Employee_ID':'Empleados'}, inplace = True)
+st.bar_chart (bar_values)
+
+
+# Deserción por ciudad
+st.subheader('Deserción por Ciudad de Origen')
+bar_values = data.loc[:,['Hometown','Attrition_rate']].groupby('Hometown').mean()
+st.bar_chart (bar_values)
+
+
+# Deserción por edades
+st.subheader('Deserción por Edad')
+bar_values = data.loc[:,['Age','Attrition_rate']].groupby('Age').mean()
+st.bar_chart (bar_values)
+
+# Deserción por Tiempo de servicio
+st.subheader('Deserción por Tiempo de servicio')
+bar_values = data.loc[:,['Time_of_service','Attrition_rate']].groupby('Time_of_service').mean()
+st.bar_chart (bar_values)
+
+
